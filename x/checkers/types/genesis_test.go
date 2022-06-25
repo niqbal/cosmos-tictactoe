ad00d1d984d567166a8601be45671980961e6bed
Nawab Iqbal <khichi@gmail.com>
2022-06-25 22:55:15 +0500

    next game

--- x/checkers/types/genesis_test.go
+++ x/checkers/types/genesis_test.go
@@ -19,9 +19,13 @@ func TestGenesisState_Validate(t *testing.T) {
 			valid:    true,
 		},
 		{
-			desc:     "valid genesis state",
+			desc: "valid genesis state",
 			genState: &types.GenesisState{
 
+				NextGame: &types.NextGame{
+					Creator: "95",
+					IdValue: 23,
+				},
 				// this line is used by starport scaffolding # types/genesis/validField
 			},
 			valid: true,
