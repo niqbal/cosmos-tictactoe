7d89fd31f9de7f71a8635f9e7b40e74f28123c49
Nawab Iqbal <khichi@gmail.com>
2022-06-25 22:59:49 +0500

    full game and errors file

--- x/checkers/types/errors.go
+++ x/checkers/types/errors.go
@@ -8,5 +8,11 @@ import (
 
 // x/checkers module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrInvalidCreator      = sdkerrors.Register(ModuleName, 1100, "creator address is invalid: %s")
+	ErrGameNotParseable    = sdkerrors.Register(ModuleName, 1101, "game cannot be parsed")
+	ErrGameNotFound        = sdkerrors.Register(ModuleName, 1102, "game by id not found: %s")
+	ErrCreatorNotPlayer    = sdkerrors.Register(ModuleName, 1103, "message creator is not a player: %s")
+	ErrTurnAddressNotValid = sdkerrors.Register(ModuleName, 1104, "message turn is not a player: %s")
+	ErrNotPlayerTurn       = sdkerrors.Register(ModuleName, 1105, "player tried to play out of turn: %s")
+	ErrWrongMove           = sdkerrors.Register(ModuleName, 1106, "wrong move")
 )
